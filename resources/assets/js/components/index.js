107421266cb482e605c95704d44b3ea1c285aa38
Pham Tien Dat <phamdat22396@gmail.com>
2018-06-05 14:19:12 +0700

    dat commit

--- resources/assets/js/components/index.js
+++ resources/assets/js/components/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import Example from "./Example";
+import HeaderComponent from "./HeaderComponent";
 
 export default () => {
     return (
-        <Example />
+        // <Example />
+        <HeaderComponent />
     )
 }
