def2ef3da4ce9d1541c232dbe0d4a4fe3330c590
Pham Tien Dat <phamdat22396@gmail.com>
2018-06-06 09:02:38 +0700

    dat commit

--- app/Http/Controllers/Rest/UserCtrl.php
+++ app/Http/Controllers/Rest/UserCtrl.php
@@ -5,8 +5,68 @@ namespace App\Http\Controllers\Rest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\UserModel;
+use App\Libs\Config\UserConfig;
 
 class UserCtrl extends Controller
 {
-    //
+    private $userModel;
+
+    public function __construct(UserModel $userModel){
+    	$this->userModel = $userModel;
+    }
+
+    //get list user
+    public function listUser(Request $request){
+
+		$freeText = $request->input("freeText", "");
+		$name     = $request->input("name", "");
+		$email    = $request->input("email", "");
+		$phone    = $request->input("phone", "");
+		$status   = $request->input("status", UserConfig::CONST_USER_AVAILABLE);
+		$perPage  = $request->input("perPage", "");
+
+		$listUser = $this->userModel->filterFreeText($freeText)
+						->filterName($name)
+						->filterEmail($email)
+						->filterPhone($phone)
+						->filterStatus($status)
+						->paginate($perPage);
+
+		return response()->json($listUser);  
+    }
+
+    //insert User
+    public function insertUser(Request $request){
+
+		$name     = $request->input("name", "");
+		$email    = $request->input("email", "");
+		$phone    = $request->input("phone", "");
+		$account  = $request->input("account", "");
+		$phone    = $request->input("phone", "");
+		$phone    = $request->input("phone", "");
+
+		return response()->json($listUser);  
+    }
+
+
+    //validateInser data request
+    public function validateInser($request){
+	    return $this->validate($request, [
+				'name'   => 'required',
+				'acount' => 'required|unique:users',
+				'email'  => 'required|email|unique:users',
+				'phone'  => 'required',
+				'status' => 'required',
+			], [
+				'name.required'   => 'Tên người dùng không được để trống',
+				'acount.required' => 'Tài khoản không được để trống',
+				'email.required'  => 'email không được để trống',
+				'phone.required'  => 'Điện thoại không được để trống',
+				'status.required' => 'Trạng thái không được để trống',
+				'acount.unique'   => 'Tài khoản đã tồn tại',
+				'email.email'     => 'Email không đúng định dạng',
+				'email.unique'    => 'Email đã tồn tại',
+	    	]
+		);
+	}
 }
---
+++ app/Libs/Config/UserConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Libs\Config;
+
+class UserConfig{
+    
+	const CONST_USER_AVATAR    = 'new-user-image-default.png';
+	const CONST_USER_PASSWORD  = '123456';
+	const CONST_USER_AVAILABLE = 'AVAILABLE';
+	const CONST_USER_DISABLE = 'DISABLE';
+	
+    function __construct() {
+        
+    }
+    
+}
